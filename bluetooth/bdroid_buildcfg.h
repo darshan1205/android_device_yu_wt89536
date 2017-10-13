1b732c32175ec0275186677998f9fded87176bf4
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-10-13 17:46:59 +0200

    wt89536: Update BT name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi Note 4"
+#define BTM_DEF_LOCAL_NAME   "YU YUREKA2"
 
 #define BLE_VND_INCLUDED TRUE
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
